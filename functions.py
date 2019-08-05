99e71003fc4b4e58dcdcf3046b7f22fc4ae2a81c
Andrei Ruã de Avila Silva <52323587+andreeiruan@users.noreply.github.com>
2019-08-04 21:16:04 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,32 +1,47 @@
-def att_tab(tabuleiro=None, jogada=None, quem=None):
+def att_tab(jogada, quem, tabuleiro):
     """
-    :param jogada: Recebe a opção que o jogador escolheu para marcar no tabuleiro
+    Função para atualizar o tabuleiro a cada jogada
+    :param jogada: Recebe o numero da jogada
     :param quem: Recebe quem fez a jogada
-    :param tabuleiro: Recebe o tabuleiro
-    :return: Retorna a atualização do tabuleiro a cada rodada
+    :param tabuleiro: Recebe o tabuleiro da partida
+    :return:
     """
     for item in tabuleiro:
         if item == jogada:
             tabuleiro.remove(item)
             tabuleiro.insert(jogada-1, quem)
-    return f'''
-            \t {tabuleiro[0]} | {tabuleiro[1]} | {tabuleiro[2]} 
-            \t---|---|---
-            \t {tabuleiro[3]} | {tabuleiro[4]} | {tabuleiro[5]}   
-            \t---|---|---
-            \t {tabuleiro[6]} | {tabuleiro[7]} | {tabuleiro[8]}'''
+
+
+def tab(tabuleiro):
+    """
+    Uma simples função para mostrar o tabuleiro da partida em forma de jogo da velha
+    :param tabuleiro: Recebe o tabuleiro da partida
+    :return:
+    """
+    print(f'''\n
+            \n
+            \t\t\t {tabuleiro[0]} | {tabuleiro[1]} | {tabuleiro[2]} 
+            \t\t\t---|---|---
+            \t\t\t {tabuleiro[3]} | {tabuleiro[4]} | {tabuleiro[5]}   
+            \t\t\t---|---|---
+            \t\t\t {tabuleiro[6]} | {tabuleiro[7]} | {tabuleiro[8]}
+            \n
+            \n''')
 
 
 def verificar(t):
     """
-    A função verificara as posiçoes das jogadas e se tiver uma combinação para vitoria
-    :param t: O tabuleiro do jogo que esta em uma lista.
-    :return: variavel ok para dar o fim do jogo quando houver um ganhador retornando ok = true,
-     ou ok=false enquanto não houver a combinação de vitoria
+    Função para verificar o tabueleiro, se há ou não um ganhador
+    :param t: Recebe o tabuleiro como parametro para analise das jogadas
+    :return: Ok recebe true caso haja um ganhador e encerrar a partida
     """
-    if t[0] == t[1] and t[1] == t[2]:
+    if (t[0] == t[1] and t[1] == t[2]) or (t[0] == t[3] and t[3] == t[6]) or (t[0] == t[4] and t[4] == t[8]):
         ok = True
         print(f'"{t[0]}" GANHOU!')
+        return ok
+    elif (t[2] == t[5] and t[5] == t[8]) or (t[2] == t[4] and t[4] == t[6]):
+        ok = True
+        print(f'"{t[2]}" GANHOU!')
         return ok
     elif t[3] == t[4] and t[4] == t[5]:
         ok = True
@@ -36,25 +51,9 @@ def verificar(t):
         ok = True
         print(f'"{t[6]}" GANHOU!')
         return ok
-    elif t[0] == t[3] and t[3] == t[6]:
-        ok = True
-        print(f'"{t[0]}" GANHOU!')
-        return ok
     elif t[1] == t[4] and t[4] == t[7]:
         ok = True
         print(f'"{t[1]}" GANHOU!')
-        return ok
-    elif t[2] == t[5] and t[5] == t[8]:
-        ok = True
-        print(f'"{t[2]}" GANHOU!')
-        return ok
-    elif t[2] == t[4] and t[4] == t[6]:
-        ok = True
-        print(f'"{t[2]}" GANHOU!')
-        return ok
-    elif t[0] == t[4] and t[4] == t[8]:
-        ok = True
-        print(f'"{t[0]}" GANHOU!')
         return ok
     else:
         ok = False
